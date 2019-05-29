a7b28543136c99aff94fb7ee3673bcf61a0e36c8
Shakurri <puz,show40716@gmail.com>
2019-05-30 00:24:40 +0900

    bousou

--- script.js
+++ script.js
@@ -89,3 +89,53 @@ function kakikae(){
         });
     });
   });
+   $(function(){
+    $(window).on("load scroll resize",function(){
+        $(".scrollF").each(function(){
+            var imgPos=$(this).offset().top;
+            var scroll=$(window).scrollTop();
+            var windowHeight=$(window).height();
+            if((scroll>imgPos-windowHeight+windowHeight/2)&&(already==0)){
+                already++;
+                $("html,body").animate({scrollTop:$('.imgZZZ').offset().top},2000);
+                $('.bousou1').t({
+                delay:0, //アニメーションの遅延
+                speed:35, //アニメーションの速度
+                speed_vary:false, //リアルなタイピングのスピード
+                beep:false, //タイピング音の有無
+                mistype:true, //ミスタイプの有無
+                locale:'en', //キーボードレイアウト。'en' (english) もしくは 'de' (german)
+                caret:false, //カーソル
+                blink:false, //カーソルの点滅の有無
+                blink_perm:false, //カーソルの点滅の継続
+                repeat:0, //繰り返し
+                tag:'span', //要素を内包するタグ
+                pause_on_click:false, //クリックで一時停止
+                init:function(elm){}, //タイピング開始時のコールバック
+                typing:function(elm,chr_or_elm,left,total){}, //タイピング毎のコールバック
+                fin:function(){
+                    $('.bousou2').t({
+                delay:0, //アニメーションの遅延
+                speed:10, //アニメーションの速度
+                speed_vary:false, //リアルなタイピングのスピード
+                beep:false, //タイピング音の有無
+                mistype:true, //ミスタイプの有無
+                locale:'en', //キーボードレイアウト。'en' (english) もしくは 'de' (german)
+                caret:false, //カーソル
+                blink:false, //カーソルの点滅の有無
+                blink_perm:false, //カーソルの点滅の継続
+                repeat:0, //繰り返し
+                tag:'span', //要素を内包するタグ
+                pause_on_click:false, //クリックで一時停止
+                init:function(elm){}, //タイピング開始時のコールバック
+                typing:function(elm,chr_or_elm,left,total){}, //タイピング毎のコールバック
+                fin:function(){
+                    
+                } //タイピング終了時のコールバック
+              });
+                } //タイピング終了時のコールバック
+              });
+            };
+        });
+    });
+  });
