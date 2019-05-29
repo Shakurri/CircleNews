6f937d32501b08ec07cbf7132314833ee057c81f
Shakurri <puz,show40716@gmail.com>
2019-05-30 01:27:16 +0900

    daibutsu

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function kakikae(){
     init:function(elm){}, //タイピング開始時のコールバック
     typing:function(elm,chr_or_elm,left,total){}, //タイピング毎のコールバック
     fin:function(elm){} //タイピング終了時のコールバック
-  });
+});
 };
 
 
@@ -64,8 +64,8 @@ function kakikae(){
   //       });
   //   });
   // });
- 
-   $(function(){
+
+  $(function(){
     $(window).scroll(function(){
         $(".headline").each(function(){
             var imgPos=$(this).offset().top;
@@ -76,8 +76,8 @@ function kakikae(){
             };
         });
     });
-  });
-   $(function(){
+});
+  $(function(){
     $(window).scroll(function(){
         $(".headline").each(function(){
             var imgPos=$(this).offset().top;
@@ -88,8 +88,8 @@ function kakikae(){
             };
         });
     });
-  });
-   $(function(){
+});
+  $(function(){
     $(window).on("load scroll resize",function(){
         $(".scrollF").each(function(){
             var imgPos=$(this).offset().top;
@@ -100,7 +100,7 @@ function kakikae(){
                 $("html,body").animate({scrollTop:$('.imgZZZ').offset().top},2000);
                 $('.bousou1').t({
                 delay:0, //アニメーションの遅延
-                speed:35, //アニメーションの速度
+                speed:25, //アニメーションの速度
                 speed_vary:false, //リアルなタイピングのスピード
                 beep:false, //タイピング音の有無
                 mistype:true, //ミスタイプの有無
@@ -130,12 +130,40 @@ function kakikae(){
                 init:function(elm){}, //タイピング開始時のコールバック
                 typing:function(elm,chr_or_elm,left,total){}, //タイピング毎のコールバック
                 fin:function(){
-                    
+
                 } //タイピング終了時のコールバック
-              });
+            });
                 } //タイピング終了時のコールバック
-              });
+            });
             };
         });
     });
-  });
+});
+
+  $(function(){
+    $('.resetB').on('click',function(){
+        $("html,body").animate({scrollTop:$('.headline').offset().top},5000);
+        $(".reset").fadeIn(5010);
+        $('.bousou2').t({
+                delay:0, //アニメーションの遅延
+                speed:10, //アニメーションの速度
+                speed_vary:false, //リアルなタイピングのスピード
+                beep:false, //タイピング音の有無
+                mistype:true, //ミスタイプの有無
+                locale:'en', //キーボードレイアウト。'en' (english) もしくは 'de' (german)
+                caret:false, //カーソル
+                blink:false, //カーソルの点滅の有無
+                blink_perm:false, //カーソルの点滅の継続
+                repeat:0, //繰り返し
+                tag:'span', //要素を内包するタグ
+                pause_on_click:false, //クリックで一時停止
+                init:function(elm){}, //タイピング開始時のコールバック
+                typing:function(elm,chr_or_elm,left,total){}, //タイピング毎のコールバック
+                fin:function(){} //タイピング終了時のコールバック
+            });
+        setTimeout(function(){
+            window.location.href="CircleNews2.html";
+           
+        },5000)
+    });
+});
